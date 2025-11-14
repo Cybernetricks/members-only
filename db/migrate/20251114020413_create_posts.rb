85421a8d66d3ffe3f439b7f67a08a7e925d6b355
Cybernetricks <mcgbradley@gmail.com>
2025-11-14 02:12:01 +0000

    Created_user_and_post_database_tables_and_models

---
+++ db/migrate/20251114020413_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.string :body
+      t.integer :user_id
+
+      t.timestamps
+    end
+    add_foreign_key :posts, :users, column: :user_id
+  end
+end
