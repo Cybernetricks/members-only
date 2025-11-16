c8c374a5abe635237509a03dad2609748cead6cd
Cybernetricks <mcgbradley@gmail.com>
2025-11-16 01:55:57 +0000

    Added_actions_and_validate_user_logged_in_before_post_actions

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,22 @@
 class PostController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to root_url
+    end
+  end
 end
