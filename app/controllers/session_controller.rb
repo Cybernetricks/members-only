66dee59af37b7f57940b4d26d598a0c478317e3f
Cybernetricks <mcgbradley@gmail.com>
2025-11-19 23:36:35 +0000

    Adjusted_session_controller_with_valid_methods

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -4,9 +4,9 @@ class SessionController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    if user && user.valid_password?(params[:session][:password])
+      sign_in user
+      # params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       flash[:success] = 'Logged in.'
       redirect_to root_url
     else
