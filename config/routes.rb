db53cc39c75343b5ac42061b00853cea9c315f10
Cybernetricks <mcgbradley@gmail.com>
2025-11-19 22:39:54 +0000

    WIP_added_routes_for_logins_&_logouts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :post, only: [ :new, :create, :index ]
+  get '/login', to: 'session#new'
+  post '/login', to: 'session#create'
+  delete '/logout', to: 'session#destroy'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
